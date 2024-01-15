4d19d3f539613fe8c5e1db951798e583cf60881f
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2024-01-15 16:21:17 -0300

    Executando comandos do git

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO.Compression;
 
 
@@ -10,7 +11,19 @@ namespace packmaker
         {
             Console.WriteLine("Iniciando zip!");
             string? pathToZip = "C:\\inetpub\\wwwroot\\SH0743_GSS_HOSP\\_includes\\Templates";
+            string? repo = "C:\\inetpub\\wwwroot\\SH0743_GSS_HOSP";
             string? pathToExit = "C:\\dev\\Projects\\testezip.zip";
+
+            string? gitCommand = "git";
+            string? gitChangeBr = @"checkout main"; 
+            string? gitPull = @"pull";
+            Process.Start(gitCommand, gitPull);
+
+            Process.Start(new ProcessStartInfo(gitCommand, gitChangeBr)
+            {
+                WorkingDirectory = repo
+            })
+            .WaitForExit();
 
             ZipFile.CreateFromDirectory(pathToZip, pathToExit);
 
