34f8cd654972e11d4d050bb4ccc08acd9ab18aa2
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2024-01-15 15:57:12 -0300

    Versão inicial - Zipando uma pasta com todos os arquivos e direcionando para uma saida

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO.Compression;
+
+
+namespace packmaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando zip!");
+            string? pathToZip = "C:\\inetpub\\wwwroot\\SH0743_GSS_HOSP\\_includes\\Templates";
+            string? pathToExit = "C:\\dev\\Projects\\testezip.zip";
+
+            ZipFile.CreateFromDirectory(pathToZip, pathToExit);
+
+            Console.WriteLine("Zip completo!"); 
+
+
+        }
+    }
+}
