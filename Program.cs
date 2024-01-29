3854418ae2cdff35f60398b87ac730eead379946
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2024-01-29 15:55:10 -0300

    [EM DESENVOLVIMENTO] - Gerando arquivo com chamadas de procedures

--- Program.cs
+++ Program.cs
@@ -9,23 +9,41 @@ namespace packmaker
     {
         static void Main()
         {
-            //Console.Write("Digite o caminho para o repositório Git: ");
-            string repoPath = @"C:/inetpub/wwwroot/SH0743_GSS_HOSP";  //Console.ReadLine();
+            Console.Write("Digite o caminho para o repositório Git: ");
+            string repoPath = Console.ReadLine();
             Console.Write("Digite a branch que deseja comparar: ");
             string branch = Console.ReadLine();
-            Console.Write("Digite o caminho para a saída do zip: "); 
+            Console.Write("Digite o caminho para a saída do zip: ");
             string finalZipPath = Console.ReadLine();
             Console.Write("Digite o nome do arquivo zip: ");
             string zipFile = Console.ReadLine();
+            Console.Write("Digite o nome do arquivo que vai conter as procedures: ");
+            string txtFileName = Console.ReadLine();
+
+            //string repoPath = @"C:/inetpub/wwwroot/SH0743_GSS_HOSP";
+            //string branch = "main";
+            //string finalZipPath = @"C:/Dev/packmaker_out";
+            //string zipFile = "packmaker_testZip";
+            //string txtFileName = "proceduresFound";
 
             Console.WriteLine("Obtendo arquivos....");
-            List<string> modifiedFiles = GetModifiedFiles(repoPath, branch); 
+            List<string> modifiedFiles = GetModifiedFiles(repoPath, branch);
+            List<string> aspFiles = modifiedFiles.Where(file => file.EndsWith(".asp", StringComparison.OrdinalIgnoreCase)).ToList();
             
             if(modifiedFiles.Count == 0)
             {
                 Console.WriteLine("Nenhum arquivo obtido. Programa encerrado.");
                 return;
             }
+
+            Console.WriteLine("Procurando por chamadas de procedures...");
+
+            List<string> procedureCalls = FindProcedureCalls(repoPath, aspFiles);
+
+            Console.WriteLine($"Chamadas de procedures encontradas, serão adicionadas ao arquivo: {txtFileName}.txt");
+
+            string txtPathFile = Path.Combine(finalZipPath, $"{txtFileName}.txt");
+            File.AppendAllLines(txtPathFile, procedureCalls); 
 
             Console.WriteLine("Iniciando montagem do zip....");
 
@@ -63,19 +81,31 @@ namespace packmaker
             return modifiedFiles;
 
         }
-        static void CreateZipFile(List<string> filePaths, string zipPath, string zipFileName)
+
+        static List<string> FindProcedureCalls(string repository, List<string> aspFilesList)
         {
-
-            string fullZipPath = zipPath + $"\\{zipFileName}.zip";
-
-            var zip = ZipFile.Open(fullZipPath, ZipArchiveMode.Create);
-
-            foreach (var file in filePaths)
+            List<string> procedureCalls = new List<string>();
+            foreach (var file in aspFilesList)
             {
-                zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                string aspFilePath = Path.Combine(repository, file);
+                try
+                {
+                    string[] lines = File.ReadAllLines(aspFilePath); 
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string line = lines[i];
+                        if(line.Contains("p_", StringComparison.OrdinalIgnoreCase))
+                        {
+                            procedureCalls.Add($"Arquivo: {file}, Linha: {i + 1 }, Conteudo: {line.Trim()}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao ler o arquivo {aspFilePath}: {ex.Message}");
+                }
             }
-            zip.Dispose();
-
+            return procedureCalls;
         }
 
         static void CriarZip(string rootPath, List<string> files, string zipFilePath, string zipFileName)
