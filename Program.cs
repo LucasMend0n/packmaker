33d8d0dacf2846c0a4a0726454b8950fab7490d4
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2024-01-29 14:54:14 -0300

    Criação do pack de arquivos modificados entre branches

--- Program.cs
+++ Program.cs
@@ -9,144 +9,64 @@ namespace packmaker
     {
         static void Main()
         {
-            Console.Write("Digite o caminho para o repositório Git: ");
-            string repoPath = Console.ReadLine();
-            SetBranchMain(repoPath);
+            //Console.Write("Digite o caminho para o repositório Git: ");
+            string repoPath = @"C:/inetpub/wwwroot/SH0743_GSS_HOSP";  //Console.ReadLine();
             Console.Write("Digite a branch que deseja comparar: ");
             string branch = Console.ReadLine();
-
-            ExecuteGitDiff(repoPath, branch);
-
-
-
-        }
-        static void SetBranchMain(string repoPath)
-        {
-            Console.WriteLine("Mudando para branch main...");
-            try
-            {
-                Process ps = new Process();
-                ProcessStartInfo stInfo = new ProcessStartInfo
-                {
-                    FileName = "git",
-                    Arguments = $"checkout main",
-                    WorkingDirectory = repoPath,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-
-                ps.StartInfo = stInfo;
-                ps.Start();
-
-                string output = ps.StandardOutput.ReadToEnd();
-                string error = ps.StandardError.ReadToEnd();
-
-                ps.WaitForExit();
-
-                if (!string.IsNullOrEmpty(error))
-                {
-                    Console.WriteLine("Erro ao mudar de branch: ");
-                    Console.WriteLine(error);
-                    return;
-                }
-                Console.WriteLine("Branch alterada para main! ");
-
-            }
-            catch (System.Exception)
-            {
-
-                throw;
-            }
-        }
-        static void ExecuteGitDiff(string repoPath, string branch)
-        {
-            try
-            {
-                Process process = new Process();
-                ProcessStartInfo startInfo = new ProcessStartInfo
-                {
-                    FileName = "git",
-                    Arguments = $"diff --name-only {branch}",
-                    WorkingDirectory = repoPath,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-
-                process.StartInfo = startInfo;
-                process.Start();
-
-
-                List<string> filePaths = new List<string>();
-                StreamReader stdout = process.StandardOutput;
-
-                while(!stdout.EndOfStream){
-                    filePaths.Add($"{repoPath}\\{stdout.ReadLine().Replace("/","\\")}"); 
-                }
-                
-                string error = process.StandardError.ReadToEnd();
-
-                process.WaitForExit();
-
-                Console.WriteLine($"Arquivos modificados na branch: {branch}");
-                filePaths.ForEach(i => Console.WriteLine(i));
-
-                Console.WriteLine($"Entrando na branch {branch}..."); 
-
-                Process psChangeBranch = new Process();
-                ProcessStartInfo srtInfoChangeBranch = new ProcessStartInfo
-                {
-                    FileName = "git",
-                    Arguments = $"checkout {branch}",
-                    WorkingDirectory = repoPath,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-
-                psChangeBranch.StartInfo = srtInfoChangeBranch;
-                psChangeBranch.Start();
-
-                psChangeBranch.WaitForExit();
-
-                if (!string.IsNullOrEmpty(error))
-                {
-                    Console.WriteLine("Erro ao executar o comando Git Diff:");
-                    Console.WriteLine(error);
-                    return;
-                }
-
-                Console.WriteLine("Informe o destino para o zip: ");
-                string pathForZip = Console.ReadLine();
-                
-                if (string.IsNullOrEmpty(pathForZip))
-                {
-                    Console.WriteLine("Path não selecionao, encerrando pathmaker...");
-                    return;
-                }
-
-                CreateZipFile(filePaths, pathForZip); 
-
-            }
-            
-            catch (Exception ex)
-            {
-                Console.WriteLine("Erro ao executar o comando Git Diff:");
-                Console.WriteLine(ex.Message);
-            }
-        }
-        static void CreateZipFile(List<String> filePaths, string zipPath)
-        {
-            Console.WriteLine("Digite o nome do arquivo zip: ");
+            Console.Write("Digite o caminho para a saída do zip: "); 
+            string finalZipPath = Console.ReadLine();
+            Console.Write("Digite o nome do arquivo zip: ");
             string zipFile = Console.ReadLine();
 
-            string fullZipPath = zipPath + $"\\{zipFile}.zip";
+            Console.WriteLine("Obtendo arquivos....");
+            List<string> modifiedFiles = GetModifiedFiles(repoPath, branch); 
+            
+            if(modifiedFiles.Count == 0)
+            {
+                Console.WriteLine("Nenhum arquivo obtido. Programa encerrado.");
+                return;
+            }
 
-            Console.WriteLine("Iniciando compressão dos arquivos....");
+            Console.WriteLine("Iniciando montagem do zip....");
+
+            CriarZip(repoPath, modifiedFiles, finalZipPath, zipFile);
+
+            Console.WriteLine("Compressão dos arquivos finalizada!");
+
+            string zipFolder = finalZipPath.Replace("/","\\");
+
+            Process.Start("explorer.exe", zipFolder);
+
+        }
+        static List<string> GetModifiedFiles(string repoPath, string branch)
+        {
+            List<string> modifiedFiles = new List<string>();
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = "git";
+                process.StartInfo.Arguments = $"diff --name-only ..{branch}"; 
+                process.StartInfo.WorkingDirectory = repoPath;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start(); 
+                
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    string filePath = process.StandardOutput.ReadLine();
+                    modifiedFiles.Add(filePath);
+                }
+                process.WaitForExit(); 
+;            }
+            Console.WriteLine("Arquivos obtidos!");
+            return modifiedFiles;
+
+        }
+        static void CreateZipFile(List<string> filePaths, string zipPath, string zipFileName)
+        {
+
+            string fullZipPath = zipPath + $"\\{zipFileName}.zip";
 
             var zip = ZipFile.Open(fullZipPath, ZipArchiveMode.Create);
 
@@ -156,9 +76,20 @@ namespace packmaker
             }
             zip.Dispose();
 
-            Console.WriteLine("Compressão dos arquivos finalizada!");
+        }
 
+        static void CriarZip(string rootPath, List<string> files, string zipFilePath, string zipFileName)
+        {
+            using (var zipArchive = ZipFile.Open(zipFilePath + $"\\{zipFileName}.zip", ZipArchiveMode.Create))
+            {
+                foreach (var file in files)
+                {
+                    string filePath = Path.Combine(rootPath, file);
+                    string relativePath = Path.GetRelativePath(rootPath, filePath);
+
+                    zipArchive.CreateEntryFromFile(filePath, relativePath);
+                }
+            }
         }
     }
-
 }
